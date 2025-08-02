61e0e7b80fd08014d7dcc9993dd810aa8f0e0076
King associate' s <kingassociates426@gmail.com>
2025-08-02 14:48:18 +0000

    Error: In HTML, <a> cannot be a descendant of <a>. This will cause a hyd

--- src/components/home/CreateRoomDialog.tsx
+++ src/components/home/CreateRoomDialog.tsx
@@ -6,7 +6,7 @@ import { Plus } from "lucide-react";
 
 export function CreateRoomDialog() {
   return (
-    <Link href="/video/add" passHref>
+    <Link href="/video/add" passHref legacyBehavior>
         <Button asChild className="fixed bottom-28 right-4 md:bottom-8 md:right-8 h-16 w-16 rounded-full shadow-lg shadow-primary/30 z-50">
           <a>
             <Plus className="h-8 w-8" />
