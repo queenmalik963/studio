c12946df0086ef1bd2d06a077d4d39c07727b802
King associate' s <kingassociates426@gmail.com>
2025-08-02 14:49:29 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/home/CreateRoomDialog.tsx
+++ src/components/home/CreateRoomDialog.tsx
@@ -6,13 +6,11 @@ import { Plus } from "lucide-react";
 
 export function CreateRoomDialog() {
   return (
-    <Link href="/video/add" passHref legacyBehavior>
-        <Button asChild className="fixed bottom-28 right-4 md:bottom-8 md:right-8 h-16 w-16 rounded-full shadow-lg shadow-primary/30 z-50">
-          <a>
-            <Plus className="h-8 w-8" />
-            <span className="sr-only">Create Room</span>
-          </a>
-        </Button>
-    </Link>
+    <Button asChild className="fixed bottom-28 right-4 md:bottom-8 md:right-8 h-16 w-16 rounded-full shadow-lg shadow-primary/30 z-50">
+      <Link href="/video/add">
+        <Plus className="h-8 w-8" />
+        <span className="sr-only">Create Room</span>
+      </Link>
+    </Button>
   );
 }
