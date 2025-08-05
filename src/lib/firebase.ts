8fefb16926af5e2f912b911f5e20cfe2ca057074
King associate' s <kingassociates426@gmail.com>
2025-08-05 18:15:44 +0000

    Ab

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -24,12 +25,12 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // IMPORTANT: Replace this with your own Firebase project configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY_HERE",
-  authDomain: "YOUR_AUTH_DOMAIN_HERE",
-  projectId: "YOUR_PROJECT_ID_HERE",
-  storageBucket: "YOUR_STORAGE_BUCKET_HERE",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID_HERE",
-  appId: "YOUR_APP_ID_HERE"
+  apiKey: "AlzaSyDbePWebF5_5k2AN61X0pwr2oyL6yrkqBk",
+  authDomain: "ravewaveapp.firebaseapp.com",
+  projectId: "ravewaveapp",
+  storageBucket: "ravewaveapp.appspot.com",
+  messagingSenderId: "308572836717",
+  appId: "YOUR_APP_ID_HERE" // Please get this from Firebase Console -> Project Settings -> Your Apps -> Config
 };
 
 // Initialize Firebase
