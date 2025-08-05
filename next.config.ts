cc33ee567fd000eb52d0bac5932ce1a1d994a055
King associate' s <kingassociates426@gmail.com>
2025-08-05 00:57:41 +0000

    Error: Invalid src prop (https://i.ytimg.com/vi/jfKfPfyJRdk/hqdefault_li

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         hostname: 'i.imgur.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
