4328a666d89c950cbc5623406776dfc2f2449f27
King associate' s <kingassociates426@gmail.com>
2025-08-06 08:31:10 +0000

    Perfect 👌 Main dono setup ready kar deta hoon — tum jis par chaaho depl

---
+++ src/next.config.ts
@@ -0,0 +1,47 @@
+
+// Load environment variables from .env file at the very beginning
+import { config } from 'dotenv';
+config({ path: '.env' });
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'em-content.zobj.net',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+};
+
+export default nextConfig;
