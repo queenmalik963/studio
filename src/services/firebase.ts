a844255e601a9a6dbffa48d9c0113e42b6859e98
queenmalik963 <queenmalik963@gmail.com>
2025-08-09 17:29:05 +0400

    Update firebase.ts
    Signed-off-by: queenmalik963 <queenmalik963@gmail.com>

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,2 +1,22 @@
-content
-src/services/firebase.ts
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDbePWebF5_5k2AN61X0pwr2oyL6yrkqBk",
+    authDomain: "ravewaveapp.firebaseapp.com",
+    databaseURL: "https://ravewaveapp-default-rtdb.firebaseio.com",
+    projectId: "ravewaveapp",
+    storageBucket: "ravewaveapp.appspot.com",
+    messagingSenderId: "308572836717",
+    appId: "1:308572836717:web:973439037c76af4ed6c064",
+    measurementId: "G-V60FP7V7CM"
+
+  // Initialize Firebase
+  const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Export the necessary Firebase services for the rest of the kingdom to use.
+export { app, auth, db };
